976fb8f390e3693e7f3ad0e9c83c5526f3db0949
Ryan Smith <r@32k.io>
2013-07-29 22:18:56 -0700

    more tests

--- mutex_test.go
+++ mutex_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestLock(t *testing.T) {
-	ttl := time.Second * 2
+	ttl := time.Second
 	m, err := NewMutex("redisync.test.1", ttl, "")
 	if err != nil {
 		t.Error(err)
@@ -16,6 +16,49 @@ func TestLock(t *testing.T) {
 	ok := m.TryLock()
 	if !ok {
 		t.Error("Expected mutex to be lockable.")
+		t.FailNow()
+	}
+	m.Unlock()
+}
+
+func TestLockLocked(t *testing.T) {
+	ttl := time.Second
+	m1, err := NewMutex("redisync.test.1", ttl, "")
+	if err != nil {
+		t.Error(err)
+	}
+	if ok := m1.TryLock(); !ok {
+		t.Error("Expected mutex to be lockable.")
+		t.FailNow()
+	}
+	m2, err := NewMutex("redisync.test.1", ttl, "")
+	if err != nil {
+		t.Error(err)
+	}
+	if ok := m2.TryLock(); ok {
+		t.Error("Expected mutex not to be lockable.")
+		t.FailNow()
+	}
+	m1.Unlock()
+}
+
+func TestUnlockOtherLocked(t *testing.T) {
+	ttl := time.Second
+	m1, err := NewMutex("redisync.test.1", ttl, "")
+	if err != nil {
+		t.Error(err)
+	}
+	if ok := m1.TryLock(); !ok {
+		t.Error("Expected mutex to be lockable.")
+		t.FailNow()
+	}
+
+	m2, err := NewMutex("redisync.test.1", ttl, "")
+	if err != nil {
+		t.Error(err)
+	}
+	if ok, _ := m2.Unlock(); ok {
+		t.Error("Expected mutex not to be unlockable.")
 		t.FailNow()
 	}
 }
